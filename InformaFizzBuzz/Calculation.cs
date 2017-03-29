54d0f70d7f3ffe1f07b983ff8885358d9a347f04
neil.rastogi <neil.rastogi@tungsten-network.com>
2017-03-29 19:47:57 +0100

    Add project files.

---
+++ InformaFizzBuzz/Calculation.cs
@@ -0,0 +1,34 @@
+﻿namespace InformaFizzBuzz
+{
+    public class Calculation : ICalculation
+    {
+        public string DisplayNumber(int number)
+        {
+            if (IsFizzBuzz(number))
+            {
+                return "fizzbuzz";
+            }
+            if (IsFizz(number))
+            {
+                return "fizz";
+            }
+            return IsBuzz(number) ? "buzz" : number.ToString();
+        }
+
+
+        private bool IsFizz(int i)
+        {
+            return (i%3 == 0);
+        }
+
+        private bool IsBuzz(int i)
+        {
+            return (i%5 == 0);
+        }
+
+        private bool IsFizzBuzz(int i)
+        {
+            return (i%15 == 0);
+        }
+    }
+}
---
+++ InformaFizzBuzz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformaFizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ICalculation calculation = new Calculation();
+
+            for (var i = 1; i <= 20; i++)
+            {
+              Console.WriteLine(calculation.DisplayNumber(i));
+            }
+        }
+    }
+}
